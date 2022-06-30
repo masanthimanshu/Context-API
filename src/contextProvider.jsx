25a8751fa5400585c7a9854801600143e001ee5d
Himanshu Masant <masanthimanshu@gmail.com>
2022-06-30 19:03:44 +0530

    Log statement edited

--- src/contextProvider.jsx
+++ src/contextProvider.jsx
@@ -6,7 +6,7 @@ export default function ContextProvider() {
   const [auth, setAuth] = useState(false);
   const update = () => setAuth(!auth);
 
-  console.log("Login state is", auth);
+  console.log("Login state is : ", auth);
 
   return (
     <Context.Provider value={{ auth, update }}>
